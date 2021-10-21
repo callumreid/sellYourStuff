3462c3e38b4957712f2a9a9b5e2dcd2eddc7ae50
Callum <callum.taylor.reid@gmail.com>
2021-10-21 15:02:30 -0700

    Added custom alert message to button when clicked

--- App.js
+++ App.js
@@ -1,26 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
+import { StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert} from 'react-native';
 
 export default function App() {
   const handlePress = () => console.log('clickity clackity hipdiddy smackity');
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text
-      onPress={handlePress}
-      >I'm a little teeny weeny itty bitty cowww who wants to be in an itty bitty pasture who loves to mooooooo
-      </Text>
-      <TouchableOpacity
-      onPress={() => console.log('image whiiiipped')}
-      >
-        <Image
-        source={{
-          width: 200,
-          height: 300,
-          uri: 'https://picsum.photos/200/300'}} />
-      </TouchableOpacity>
-
+      <Button title='clickMe'
+      onPress={() => Alert.alert('do u like cows?', 'mooooo')}
+      color='orange' />
     </SafeAreaView>
   );
 }
