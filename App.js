032713e38a41e11eeb194a1eaa6150a86ad27ddf
Callum <callum.taylor.reid@gmail.com>
2021-10-21 14:44:45 -0700

    Added image pulled from a website

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
-  let x = 1;
+  const handlePress = () => console.log('clickity clackity hipdiddy smackity');
+
   return (
     <SafeAreaView style={styles.container}>
       <Text
-      numberOfLines={1}
-      onPress={() => console.log('clickity clackity')}
-      >I'm a little itty bitty cowww who wants to be in an itty bitty pasture who loves to mooooooo</Text>
-      <StatusBar style="auto" />
+      onPress={handlePress}
+      >I'm a little teeny weeny itty bitty cowww who wants to be in an itty bitty pasture who loves to mooooooo
+      </Text>
+      <Image source={{
+        width: 200,
+        height: 300,
+        uri: 'https://picsum.photos/200/300'}} />
+
     </SafeAreaView>
   );
 }
@@ -18,6 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 });
